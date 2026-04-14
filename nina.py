885017f7b73e86dc4b914acd90b2d6268eb7b3b6
nina333 <azkanina.busrie@gmail.com>
2026-04-14 17:19:09 +0800

    commit 3

--- nina.py
+++ nina.py
@@ -4,3 +4,8 @@ print("30C ke F:", ke_fahrenheit(30))
 def ke_fahrenheit(c): return (c * 9/5) + 32
 def ke_reamur(c): return c * 4/5
 print("30C ke R:", ke_reamur(30))
+
+def ke_fahrenheit(c): return (c * 9/5) + 32
+def ke_reamur(c): return c * 4/5
+def ke_kelvin(c): return c + 273.15
+print("30C ke K:", ke_kelvin(30))
