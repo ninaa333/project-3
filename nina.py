66804a373af52e3fbc2cc28e5b7ebf867d123e53
nina333 <azkanina.busrie@gmail.com>
2026-04-14 17:21:04 +0800

    commit 5

--- nina.py
+++ nina.py
@@ -13,3 +13,9 @@ print("30C ke K:", ke_kelvin(30))
 def semua(c):
     print(f"F: {(c * 9/5) + 32}, R: {c * 4/5}, K: {c + 273.15}")
 semua(30)
+
+try:
+    c = float(input("Masukkan suhu Celsius: "))
+    print(f"F: {(c * 9/5) + 32}, R: {c * 4/5}, K: {c + 273.15}")
+except:
+    print("Gunakan angka!")
